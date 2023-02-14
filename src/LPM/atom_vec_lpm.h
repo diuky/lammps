e4b8170ee64bb013cb9288a4de11f58e3ec1ab0c
diuky <Di.Liu@uky.edu>
2023-02-14 16:49:36 -0500

    1stcommit

---
+++ src/LPM/atom_vec_lpm.h
@@ -0,0 +1,32 @@
+#ifdef ATOM_CLASS
+// clang-format off
+AtomStyle(lpm,AtomVecLpm);
+// clang-format on
+#else
+
+#ifndef LMP_ATOM_VEC_LPM_H
+#define LMP_ATOM_VEC_LPM_H
+
+#include "atom_vec.h"
+
+namespace LAMMPS_NS {
+
+class AtomVecLpm : public AtomVec {
+ public:
+  AtomVecLpm(class LAMMPS *);
+
+  void grow_pointers() override;
+  void create_atom_post(int) override;
+  void data_atom_post(int) override;
+  int property_atom(const std::string &) override;
+  void pack_property_atom(int, double *, int, int) override;
+
+ private:
+  double *rmass, *vfrac, *s0;
+  double **x0;
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
+#endif
